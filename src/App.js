abaf1ab783fa7243e5eb7721fcea7cab3cdfed3c
William <whou125@gmail.com>
2020-11-14 14:48:24 -0800

    Add unformatted component with completed hook

--- src/App.js
+++ src/App.js
@@ -7,40 +7,7 @@ import "./globals/hack-styles.scss";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 // Website imports for classes you made
-import { Card } from "./app/containers";
-
-function SampleCard() {
-  return (
-    <Card style={{ width: "30vw"}}>
-      <h1>
-        HackUCI Template
-      </h1>
-      <h4>Featuring our friends:</h4>
-      <p>
-        Create React App
-      </p>
-      <p>
-        SASS
-      </p>
-      <p>
-        React Router
-      </p>
-    </Card>
-  );
-}
-
-function OtherCard() {
-  return (
-    <Card style={{ width: "30vw"}}>
-      <h1>
-        Other Page!
-      </h1>
-      <p>
-        Howdy
-      </p>
-    </Card>
-  );
-}
+import { Task } from "./app/components"
 
 function App() {
   return (
@@ -49,11 +16,7 @@ function App() {
         <Switch>
           <Route 
             exact path={"/"}
-            component={SampleCard}
-          />
-          <Route 
-            exact path={"/other"}
-            component={OtherCard}
+            component={Task}
           />
         </Switch>
       </Router>
