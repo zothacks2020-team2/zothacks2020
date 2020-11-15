b3b8bafe9abf01e10dbe6562894d2b7aba431e2b
Daniel Koo <donghyun122600@gmail.com>
2020-11-15 06:58:56 -0800

    Added transitions for each card

--- src/app/components/songCard.js
+++ src/app/components/songCard.js
@@ -1,11 +1,20 @@
 import "./songCard.scss";
 import React from "react";
+import { motion } from "framer-motion";
 //import {NavLink} from "react-router-dom";
 
 function SongCard({data}) {
     // use data.image for src
     return (
-        <a className="song-card" href={data.url}>
+        <motion.a 
+        animate={{
+            opacity: [0, 1],
+        }}
+        transition={{
+            duration: 1.0,
+            delay: 0.5
+        }}
+        className="song-card" href={data.url}>
             <img src={data.image} alt={"album cover for " + data.song} className="album-cover">
             </img>
             <h3>
@@ -14,7 +23,7 @@ function SongCard({data}) {
             <h4>
                 {data.artist}
             </h4>
-        </a>
+        </motion.a>
     )
 }
 
--- src/app/components/todoCard.js
+++ src/app/components/todoCard.js
@@ -2,19 +2,28 @@
 import "./todoCard.scss";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
 //import { Link } from "react-router-dom";
 
-function TodoCard({ data, toggleData }) {
+function TodoCard({ data, number, toggleData }) {
     return (
-        <div className="todo-card flex-col" onClick={toggleData}>
+        <motion.div initial={{ opacity: 0 }}
+            animate={{
+                opacity: [0, 1],
+            }}
+            transition={{
+                duration: 1.0,
+                delay: data.new ? 0.4 : number * 0.4
+            }}
+            className="todo-card flex-col" onClick={toggleData}>
             <h1 className={data.completed ? "todo-completed" : ""}>{data.task}</h1>
             <div>
                 <NavLink to={{ pathname: "/recommend", state: { task: data.task } }} className="suggested">
                     Get Suggestions
                 </NavLink>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
