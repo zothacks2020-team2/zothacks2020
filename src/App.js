4e63d414418f168c9b1a2cc5197a99b8df00370d
Brett Kaplan <74386612+BrettTK@users.noreply.github.com>
2020-11-15 01:27:33 -0800

    Tried to write add_task

--- src/App.js
+++ src/App.js
@@ -11,16 +11,14 @@ import { Card } from "./app/containers";
 
 function SampleCard() {
   return (
-    <Card style={{ width: "30vw"}}>
+    <Card style={{ width: "30vw"}, { height: "50vw"}}>
       <h1>
-        HackUCI Template
+        Add To-Do List Task:
       </h1>
-      <h4>Featuring our friends:</h4>
+      <h4>Name of Task:</h4>
       <p>
-        Create React App
-      </p>
-      <p>
-        SASS
+        {/* <input type="button" value="OK">
+        <input type="text"> */}
       </p>
       <p>
         React Router
---
+++ src/app/containers/card/AddTask.js
@@ -0,0 +1,28 @@
+var tasks = []
+button = new button
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Add a To-Do Task:</h2>
+
+<form action="/action_page.php">
+  <label for="user_input">Enter a New To-Do Task:</label><br>
+  <input type="text" id="user_input" name="user_input" value="Test"><br>
+  <input type="submit" value="Submit">
+</form> 
+
+print("hello")
+
+</body>
+</html>
+
+AddTask(user_input)
+
+function AddTask({userInput}) 
+{
+    const taskName = userInput
+    tasks.append(userInput)
+    // update interface to show updated list
+}
