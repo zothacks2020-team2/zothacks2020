08c0af019a4569fe19b3f49c420de1d24ccf5533
William <whou125@gmail.com>
2020-11-15 02:11:21 -0800

    Infer music category based on task text

--- backend/spotifydatafetcher.py
+++ backend/spotifydatafetcher.py
@@ -1,14 +1,11 @@
 import spotipy
 import random
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
-
-auth_manager = SpotifyClientCredentials(
-    client_id="4a1f0d78ee9445bd9c4aeec7db23e97f", client_secret="83d982a8205641a58dccdea791bb3ab9")
-sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 class SpotifyDataFetcher():
-    def __init__(self, num_recs=5):
+    def __init__(self, num_recs=3):
         # The number of song recommendations to generate
         self.num_recs = num_recs
 
@@ -20,15 +17,26 @@ class SpotifyDataFetcher():
 
     def get_song_recommendations(self, task: str) -> dict:
         category = self.__infer_task_category(task)
-        self.category = category
+
+        # Rick roll the user if their tasks can't be mapped to a category
+        if category is not None:
+            self.category = category
+        else:
+            return [{'name': 'Never Gonna Give You Up', 'artist': 'Rick Astley', 'image': 'https://open.spotify.com/embed/album/6XhjNHCyCDyyGJRM5mg40G', 'url': 'https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb?si=RWMG19I3SpeeRIaU2e2xLQ'}]
+
         return self.__run_pipeline()
 
     def __run_pipeline(self):
-        self.__authenticate()
-        self.__select_category_playlist()
-        self.__select_playlist_tracks()
-        return self.__package_selected_tracks()
+        try:
+            self.__authenticate()
+            self.__select_category_playlist()
+            self.__select_playlist_tracks()
+            return self.__package_selected_tracks()
+        except Exception as e:
+            print(e)
+            return [{'name': 'Never Gonna Give You Up', 'artist': 'Rick Astley', 'image': 'https://open.spotify.com/embed/album/6XhjNHCyCDyyGJRM5mg40G', 'url': 'https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb?si=RWMG19I3SpeeRIaU2e2xLQ'}]
 
+    # mmmmmmmmmm we love exposing API keys for the world to see
     def __authenticate(self):
         auth_manager = SpotifyClientCredentials(
             client_id="4a1f0d78ee9445bd9c4aeec7db23e97f", client_secret="83d982a8205641a58dccdea791bb3ab9")
@@ -37,6 +45,7 @@ class SpotifyDataFetcher():
     def __select_category_playlist(self) -> str:
         playlists = self.spclient.category_playlists(
             self.category, country='US', limit=50)['playlists']['items']
+
         # for playlist in playlists:
         #     print(playlist['id'])
         #     print(playlist['name'])
@@ -68,4 +77,25 @@ class SpotifyDataFetcher():
         return all_tracks
 
     def __infer_task_category(self, task: str):
-        return(task)
+        task_words = task.split(' ')
+        with open('categoryMap.json') as fp:
+            category_map = json.load(fp)
+
+        category_tracker = {key: 0 for key in category_map.keys()}
+
+        for category, keywords in category_map.items():
+            for word in task_words:
+                if word in keywords:
+                    category_tracker[category] += 1
+
+        # The (category, keyword matches) pair with the most number of keyword matches. DOES NOT ACCOUNT FOR TIES
+        top_pair = sorted(category_tracker.items(),
+                          key=lambda x: x[1], reverse=True)[0]
+
+        # Return None if there are no keyword matches
+        if top_pair[1] == 0:
+            print("No matches :(")
+            return None
+        else:
+            print(f"Returning {top_pair[0]}!")
+            return top_pair[0]
